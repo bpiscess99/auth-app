06e19f76c49be63aa44a3df3a99e98a898550b2a
UMAIR <bumair97@gmail.com>
2023-01-09 23:13:09 +0500

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,9 @@ const cookieParser = require('cookie-parser');
 const userRoute = require('./routes/userRoute');
 const errorHandler = require("./middlewares/errorMiddleware");
 
-
-
 const app = express();
 
 // Middleware
-
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,7 +28,6 @@ app.get('/', (req, res) => {
   mongoose.set('strictQuery', false);
   const PORT = process.env.PORT || 5000;
   
-
   mongoose.connect(process.env.MONGO_URI)
   .then(() => {
   app.listen(PORT, () => {
