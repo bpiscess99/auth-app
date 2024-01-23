678182d968d22f17f4aef9acfdbffe440eb26c87
Umair bashir <bumair9@gmail.com>
2024-01-24 00:29:01 +0500

    home

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,12 @@ const cookieParser = require('cookie-parser');
 const userRoute = require('./routes/userRoute');
 const errorHandler = require("./middlewares/errorMiddleware");
 
+
+
 const app = express();
 
 // Middleware
+
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +31,7 @@ app.get('/', (req, res) => {
   mongoose.set('strictQuery', false);
   const PORT = process.env.PORT || 5000;
   
+
   mongoose.connect(process.env.MONGO_URI)
   .then(() => {
   app.listen(PORT, () => {
